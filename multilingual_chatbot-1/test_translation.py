16f4cb74497fc09c6a81fd6f3090de76caf9239b
LINGALA ANUSHA <161560036+lingala-anusha-23@users.noreply.github.com>
2026-01-16 10:40:00 +0530

    Delete multilingual_chatbot-1 directory

--- multilingual_chatbot-1/test_translation.py
+++
@@ -1,9 +0,0 @@
-from translatepy import Translator
-
-# Test translation
-translator = Translator()
-try:
-    result = translator.translate("Hello, how are you?", "fr")
-    print("Translation successful:", result)
-except Exception as e:
-    print("Translation error:", e)
